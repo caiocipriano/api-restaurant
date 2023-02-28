af55f5094ea214e75b1ede16155c8bd8940cafac
caiocipriano <caio.silvax@hotmail.com>
2023-02-28 10:32:43 -0300

    feat: add restaurant routes in server.ts

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,12 +1,13 @@
 import express  from "express";
 import  cors  from "cors";
 import '@shared/typeorm'
+import { routes } from "./routes";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use(routes);
 app.listen(8080, ()=>{
     console.log("Rodando...🔥");
 })
