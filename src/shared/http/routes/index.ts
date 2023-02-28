36239fde7dbb6eac95ca68372d991b80f7052705
caiocipriano <caio.silvax@hotmail.com>
2023-02-28 10:32:18 -0300

    feat: index with restaurant routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,4 +1,9 @@
+import { restaurantRouter } from "@modules/restaurant/routes/restaurant-routes";
 import { Router } from "express";
 
 const routes = Router();
 
+routes.use('/restaurants', restaurantRouter);
+console.log("1usingrestaurantroutes")
+
+export {routes};
